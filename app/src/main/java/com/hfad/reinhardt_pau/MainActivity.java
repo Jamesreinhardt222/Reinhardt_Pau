4ddbc48d44dd2979fd167a113b097a534761de09
James Reinhardt <james@Jamess-MacBook-Pro.local>
2020-10-15 18:35:02 -0700

    Fixed bug

--- app/src/main/java/com/hfad/reinhardt_pau/MainActivity.java
+++ app/src/main/java/com/hfad/reinhardt_pau/MainActivity.java
@@ -1,11 +1,9 @@
 package com.hfad.reinhardt_pau;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 
@@ -15,23 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = (Button) findViewById(R.id.submit);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), NewsListActivity.class);
-                view.getContext().startActivity(intent);}
-        });
-
     }
 
 
-//
-//    public void submitKeyword(View view) {
-//        final EditText editText = (EditText) findViewById(R.id.keyword);
-//        Intent intent = new Intent(this, NewsListActivity.class);
-//        intent.putExtra("keyword", editText.getText().toString());
-//        startActivity(intent);
-//    }
+    public void submitKeyword(View view) {
+        final EditText editText = (EditText) findViewById(R.id.keyword);
+        Intent intent = new Intent(this, NewsListActivity.class);
+        intent.putExtra("keyword", editText.getText().toString());
+        startActivity(intent);
+    }
 
 }
